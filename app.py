042cdc623d4a7901b4069a09561a87a50ab49c83
Andrew Kenworthy <akenworthy87@gmail.com>
2019-08-07 22:56:16 +0000

    Changed MongoDB password to load from enviroment variable. Made basic edit and create pages for movies

--- app.py
+++ app.py
@@ -6,28 +6,65 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'ifdb'
-#Change this to an enviro-var
-app.config["MONGO_URI"] = 'mongodb+srv://root:{}@ci-datacentricpy-e8zwz.mongodb.net/ifdb?retryWrites=true&w=majority'.format('r00tUser') 
+app.config["MONGO_URI"] = 'mongodb+srv://root:{}@ci-datacentricpy-e8zwz.mongodb.net/ifdb?retryWrites=true&w=majority'.format(os.environ.get('MONGODB_PASS')) 
 
 mongo = PyMongo(app)
 
-# Main Page
+# Main Page ####################################################################
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template("index.html", movies=mongo.db.movies.find(), actors=mongo.db.actors.find())
 
-# Movie Pages
+
+# Movie Pages ##################################################################
+## Movies list
 @app.route('/movies')
 def list_movies():
     return render_template("movies/movies.html", movies=mongo.db.movies.find())
 
+## Add movie functions
+@app.route('/add_movie')
+def add_movie():
+    return render_template('movies/createmovie.html')
+
+@app.route('/insert_movie', methods=['POST'])
+def insert_movie():
+    #validation goes here
+    movies = mongo.db.movies
+    movies.insert_one(request.form.to_dict())
+    return redirect(url_for('list_movies'))
+
+## Edit movie functions
+@app.route('/edit_movie/<movie_id>')
+def edit_movie(movie_id):
+    the_movie = mongo.db.movies.find_one({"_id": ObjectId(movie_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('movies/editmovie.html', movie=the_movie, categories=all_categories)
+
+@app.route('/update_movie/<movie_id>', methods=['POST'])
+def update_movie(movie_id):
+    movies = mongo.db.movies
+    movies.update( {'_id': ObjectId(movie_id)},
+    {
+        'movie_name': request.form.get('movie_name'),
+        'art': request.form.get('art'),
+        'released': request.form.get('released'),
+        'genre': request.form.get('genre'),
+        'runtime':request.form.get('runtime'),
+        'rating':request.form.get('rating'),
+        'plot':request.form.get('plot')
+    })
+    return redirect(url_for('list_movies'))
+
+## View Movie
 @app.route('/view_movie/<movie_id>')
 def view_movie(movie_id):
     movie_details = mongo.db.movies.find_one({"_id": ObjectId(movie_id)})
     return render_template('movies/viewmovie.html', movie=movie_details)
 
-#Actor Pages
+
+# Actor Pages ##################################################################
 @app.route('/actors')
 def list_actors():
     return render_template("actors/actors.html", actors=mongo.db.actors.find())
