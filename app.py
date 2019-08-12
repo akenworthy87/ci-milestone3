03b7a9269511d80a12e1f113870610d8849f3325
Andrew Kenworthy <akenworthy87@gmail.com>
2019-08-12 21:33:38 +0000

    Added create and edit pages for actor records

--- app.py
+++ app.py
@@ -45,8 +45,8 @@ def edit_movie(movie_id):
 @app.route('/update_movie/<movie_id>', methods=['POST'])
 def update_movie(movie_id):
     movies = mongo.db.movies
-    movies.update( {'_id': ObjectId(movie_id)},
-    {
+    movies.update_one( {'_id': ObjectId(movie_id)},
+    {"$set":{
         'movie_name': request.form.get('movie_name'),
         'art': request.form.get('art'),
         'released': request.form.get('released'),
@@ -54,7 +54,7 @@ def update_movie(movie_id):
         'runtime':request.form.get('runtime'),
         'rating':request.form.get('rating'),
         'plot':request.form.get('plot')
-    })
+    }})
     return redirect(url_for('list_movies'))
 
 ## View Movie
@@ -69,11 +69,44 @@ def view_movie(movie_id):
 def list_actors():
     return render_template("actors/actors.html", actors=mongo.db.actors.find())
 
+## Add actor functions
+@app.route('/add_actor')
+def add_actor():
+    return render_template('actors/createactor.html')
+
+@app.route('/insert_actor', methods=['POST'])
+def insert_actor():
+    #validation goes here
+    actors = mongo.db.actors
+    actors.insert_one(request.form.to_dict())
+    return redirect(url_for('list_actors'))
+
+## Edit actor functions
+@app.route('/edit_actor/<actor_id>')
+def edit_actor(actor_id):
+    the_actor = mongo.db.actors.find_one({"_id": ObjectId(actor_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('actors/editactor.html', actor=the_actor, categories=all_categories)
+
+@app.route('/update_actor/<actor_id>', methods=['POST'])
+def update_actor(actor_id):
+    actors = mongo.db.actors
+    actors.update( {'_id': ObjectId(actor_id)},
+    {"$set":{
+        'actor_name': request.form.get('actor_name'),
+        'photo': request.form.get('photo'),
+        'background': request.form.get('background')
+    }})
+    return redirect(url_for('list_actors'))
+
+## View actor
 @app.route('/view_actor/<actor_id>')
 def view_actor(actor_id):
     actor_details = mongo.db.actors.find_one({"_id": ObjectId(actor_id)})
     return render_template('actors/viewactor.html', actor=actor_details)
 
+
+# Misc #########################################################################
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
